ca6f469c355b8b16be6a89b630969b4e1e7b030f
jose2a <jose2a@10.249.77.231>
2019-06-06 13:35:13 -0400

    Implement CarService and create DAOUtils.

---
+++ src/main/java/com/revature/cardealership/services/CarServiceImpl.java
@@ -0,0 +1,34 @@
+package com.revature.cardealership.services;
+
+import java.util.List;
+
+import com.revature.cardealership.model.Car;
+
+public class CarServiceImpl implements CarService {
+	
+
+	@Override
+	public boolean addCar(Car car) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Car> getCars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeCar(String vin) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Car> getCarsByCustomerUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/revature/cardealership/utils/DAOUtils.java
@@ -0,0 +1,14 @@
+package com.revature.cardealership.utils;
+
+import com.revature.cardealership.dao.DealershipDAO;
+import com.revature.cardealership.dao.DealershipDAOImpl;
+
+public class DAOUtils {
+	
+	private static String FILE_NAME = "dealershio.dat";
+
+	public static DealershipDAO geDealershipDAO() {
+		return new DealershipDAOImpl(FILE_NAME);
+	}
+	
+}
